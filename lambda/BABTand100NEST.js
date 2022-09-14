6d1b1359a5c9b4f3f8662e2af6866d1b4cc16793
tunogya <tunogya@qq.com>
2022-09-14 11:45:12 +0800

    add BABand100NEST

---
+++ lambda/BABTand100NEST.js
@@ -0,0 +1,43 @@
+const {ethers} = require("ethers");
+const erc20abi = require("../abis/erc20.json");
+const erc721abi = require("../abis/erc721.json");
+
+const NETWORK_URL = 'https://bsc-dataseed.binance.org/'
+
+const NEST_ADDRESS = '0x98f8669f6481ebb341b522fcd3663f79a3d1a6a7'
+
+const BABT_ADDRESS = '0x2B09d47D550061f995A3b5C6F0Fd58005215D7c8'
+
+const provider = new ethers.providers.JsonRpcProvider(NETWORK_URL)
+
+const NEST = new ethers.Contract(NEST_ADDRESS, erc20abi, provider)
+const BABT = new ethers.Contract(BABT_ADDRESS, erc721abi, provider)
+
+exports.handler = async (event) => {
+  const user_id = JSON.parse(event.body)?.user_id ?? undefined
+  const wallet = JSON.parse(event.body)?.wallet ?? undefined
+  
+  if (user_id === undefined && wallet === undefined) {
+    return {
+      statusCode: 200,
+      body: false
+    }
+  }
+  
+  const [balanceOfNEST, balanceOfBABT] = await Promise.all([
+    NEST.balanceOf(wallet),
+    BABT.balanceOf(wallet)
+  ])
+  
+  if (balanceOfNEST.gte(ethers.utils.parseEther("100")) && balanceOfBABT.gte(1)) {
+    return {
+      statusCode: 200,
+      body: true,
+    };
+  }
+  
+  return {
+    statusCode: 200,
+    body: false,
+  };
+};
