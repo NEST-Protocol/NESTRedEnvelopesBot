0892b72565006504ead281ccf7e0f528356d0743
tunogya <tunogya@qq.com>
2022-10-10 18:51:36 +0800

    update

--- lambda/nest-prize-twitter-challenge.js
+++ lambda/nest-prize-twitter-challenge.js
@@ -13,7 +13,7 @@ exports.handler = async (event) => {
     }
   }
   
-  let twitterId, isLike, isRetweet
+  let twitterId
   
   try {
     const req = await axios({
@@ -23,43 +23,44 @@ exports.handler = async (event) => {
         'Authorization': `Bearer ${bearToken}`
       }
     })
-    twitterId = req.data.data.id
-  } catch (e) {
-    return {
-      statusCode: 200,
-      body: false,
-    };
-  }
-  
-  try {
-    const [liked, retweeted] = await Promise.all([
-      axios({
-        method: 'GET',
-        url: `https://api.twitter.com/2/users/${twitterId}/liked_tweets`,
-        headers: {
-          'Authorization': `Bearer ${bearToken}`
-        }
-      }),
-      axios({
-        method: 'GET',
-        url: `https://api.twitter.com/2/users/${twitterId}/tweets?max_results=50&expansions=referenced_tweets.id.author_id`,
-        headers: {
-          'Authorization': `Bearer ${bearToken}`
-        }
-      })
-    ])
+    const twitterId = req.data.data.id
     
-    const liked_list = liked.data.data || []
-    const retweeted_list = retweeted.data.data || []
-    isLike = liked_list.some((item) => item.id === tweet)
-    isRetweet = retweeted_list.some((item) => item.referenced_tweets && item.referenced_tweets[0].id === tweet)
-    if (!isLike || !isRetweet) {
+    try {
+      const [liked, retweeted] = await Promise.all([
+        axios({
+          method: 'GET',
+          url: `https://api.twitter.com/2/users/${twitterId}/liked_tweets`,
+          headers: {
+            'Authorization': `Bearer ${bearToken}`
+          }
+        }),
+        axios({
+          method: 'GET',
+          url: `https://api.twitter.com/2/users/${twitterId}/tweets?max_results=50&expansions=referenced_tweets.id.author_id`,
+          headers: {
+            'Authorization': `Bearer ${bearToken}`
+          }
+        })
+      ])
+    
+      const liked_list = liked.data.data || []
+      const retweeted_list = retweeted.data.data || []
+      const isLike = liked_list.some((item) => item.id === tweet)
+      const isRetweet = retweeted_list.some((item) => item.referenced_tweets && item.referenced_tweets[0].id === tweet)
+      if (!isLike || !isRetweet) {
+        return {
+          statusCode: 200,
+          body: false,
+        };
+      }
+    } catch (_) {
       return {
         statusCode: 200,
         body: false,
       };
     }
-  } catch (_) {
+  
+  } catch (e) {
     return {
       statusCode: 200,
       body: false,
