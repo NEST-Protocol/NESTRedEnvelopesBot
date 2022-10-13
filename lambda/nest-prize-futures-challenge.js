ba1765284722e4d56496b40681432d7e068ed8f2
tunogya <tunogya@qq.com>
2022-10-13 12:03:29 +0800

    update futures challenge

--- lambda/nest-prize-futures-challenge.js
+++ lambda/nest-prize-futures-challenge.js
@@ -4,6 +4,9 @@ const apiKey = process.env.APIKEY
 exports.handler = async (event) => {
   const user_id = JSON.parse(event?.body)?.user_id ?? undefined
   const wallet = JSON.parse(event?.body)?.wallet ?? undefined
+  const lever = event?.queryStringParameters?.lever ?? undefined
+  const amount = event?.queryStringParameters?.amount ?? undefined
+  const total = event?.queryStringParameters?.total ?? undefined
   
   if (user_id === undefined && wallet === undefined) {
     return {
@@ -18,23 +21,56 @@ exports.handler = async (event) => {
       url: `https://api.bscscan.com/api?module=account&action=txlist&address=0x8e32C33814271bD64D5138bE9d47Cd55025074CD&startblock=0&endblock=99999999&page=1&offset=1000&sort=desc&apikey=${apiKey}`,
     })
     const data = req.data.result || []
-    
-    const res = data.some((tx) => {
-      const now = new Date()
+    const now = new Date()
+    const buyTx = data.filter((tx) => {
       const txDate = new Date(tx.timeStamp * 1000)
-      
-      return tx.from.toLowerCase() === wallet.toLowerCase() &&
-          tx.methodId === '0x15ee0aad' &&
+      return tx.methodId === '0x15ee0aad' &&
+          tx.isError === '0' &&
+          tx.from.toLowerCase() === wallet.toLowerCase() &&
           now.getFullYear() === txDate.getFullYear() &&
           now.getMonth() === txDate.getMonth() &&
           now.getDate() === txDate.getDate()
     })
     
-    if (res) {
+    if (lever === undefined || amount === undefined || total === undefined) {
       return {
         statusCode: 200,
-        body: true,
-      };
+        body: buyTx.length > 0
+      }
+    }
+    
+    if (lever) {
+      return {
+        statusCode: 200,
+        body: buyTx.some((tx) => {
+          const input = tx.input
+          const txLever = parseInt(input.slice(74, 138), 16)
+          return txLever === parseInt(lever)
+        })
+      }
+    }
+    
+    if (amount) {
+      return {
+        statusCode: 200,
+        body: buyTx.some((tx) => {
+          const input = tx.input
+          const txAmount = parseInt(input.slice(202, 266), 16) / 1e18
+          return txAmount >= parseInt(amount)
+        })
+      }
+    }
+    
+    if (total) {
+      const totalAmount = buyTx.reduce((acc, tx) => {
+        const input = tx.input
+        const txAmount = parseInt(input.slice(202, 266), 16) / 1e18
+        return acc + txAmount
+      })
+      return {
+        statusCode: 200,
+        body: totalAmount >= parseInt(total)
+      }
     }
   } catch (e) {
     return {
