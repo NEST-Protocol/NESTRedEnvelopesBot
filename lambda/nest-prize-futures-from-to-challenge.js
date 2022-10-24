eacafd87e2a4bc9b6a78b16b31de9c664cb09b27
tunogya <tunogya@qq.com>
2022-10-24 17:53:12 +0800

    add api

---
+++ lambda/nest-prize-futures-from-to-challenge.js
@@ -0,0 +1,64 @@
+const axios = require("axios");
+
+const apiKey = process.env.APIKEY
+exports.handler = async (event) => {
+  const wallet = JSON.parse(event?.body)?.wallet ?? undefined
+  const total = event?.queryStringParameters?.total ?? undefined
+  const from = event?.queryStringParameters?.from ?? undefined
+  const to = event?.queryStringParameters?.to ?? undefined
+  
+  if (wallet === undefined) {
+    return {
+      statusCode: 200,
+      body: false
+    }
+  }
+  
+  try {
+    const req = await axios({
+      method: 'GET',
+      url: `https://api.bscscan.com/api?module=account&action=txlist&address=0x8e32C33814271bD64D5138bE9d47Cd55025074CD&startblock=0&endblock=99999999&page=1&offset=1000&sort=desc&apikey=${apiKey}`,
+    })
+    const data = req.data.result || []
+    const buyTx = data.filter((tx) => {
+      const txDate = new Date(tx.timeStamp * 1000)
+      return tx.methodId === '0x15ee0aad' &&
+          tx.isError === '0' &&
+          tx.from.toLowerCase() === wallet.toLowerCase() &&
+          txDate < new Date(to) &&
+          txDate > new Date(from)
+    })
+    
+    if (buyTx.length === 0) {
+      return {
+        statusCode: 200,
+        body: false
+      }
+    }
+    
+    if (total !== undefined) {
+      const totalAmount = buyTx.reduce((acc, tx) => {
+        const input = tx.input
+        const txAmount = parseInt(input.slice(202, 266), 16) / 1e18
+        return acc + txAmount
+      }, 0)
+      
+      if (totalAmount < parseInt(total)) {
+        return {
+          statusCode: 200,
+          body: false
+        }
+      }
+    }
+  } catch (e) {
+    return {
+      statusCode: 200,
+      body: false,
+    };
+  }
+  
+  return {
+    statusCode: 200,
+    body: true,
+  };
+};
