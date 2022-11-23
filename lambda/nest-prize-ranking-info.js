99dabe042d5c539b78ea2778ffd7f11cbeceeafd
tunogya <tunogya@qq.com>
2022-11-23 14:24:03 +0800

    update

---
+++ lambda/nest-prize-ranking-info.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+
+exports.handler = async () => {
+  try {
+    const res = await axios({
+      method: 'get',
+      timeout: 3000,
+      url: `https://work.parasset.top/workbench-api/activity/info/integral/ranking`,
+      headers: {
+        "Authorization": `Bearer ${process.env.BEAR_TOKEN}`
+      }
+    })
+    
+    if (res.data) {
+      return {
+        statusCode: 200,
+        body: `
+Ranking:
+
+${res.data.map((i, index) => `${index + 1}. ${i.wallet.slice(0, 4)}...${i.wallet.slice(-4, 0)} total: ${i.total}`).join("\n")}
+`
+      }
+    } else {
+      return {
+        statusCode: 200,
+        body: "No data"
+      }
+    }
+  } catch (e) {
+    return {
+      statusCode: 200,
+      body: "Error, please try again later."
+    }
+  }
+}
+;
